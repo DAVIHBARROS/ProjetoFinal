e6a89509c0e7286b56633945b01c476b21027440
LucasKrotzz <lucas-2234174@educar.rs.gov.br>
2024-06-26 20:24:41 -0300

    Alterações

--- maratona/src/main/java/com/example/maratona/Controller/InscricaoController.java
+++ maratona/src/main/java/com/example/maratona/Controller/InscricaoController.java
@@ -5,23 +5,12 @@ import com.example.maratona.Entity.Inscricao;
 import com.example.maratona.Service.CircuitoService;
 import com.example.maratona.Service.InscricaoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("api/inscricao")
-
-import com.example.maratona.Entity.Inscricao;
-
-import com.example.maratona.Service.InscricaoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @RestController
-@RequestMapping
+@RequestMapping("api/inscricao")
 public class InscricaoController {
 
     @Autowired
@@ -31,10 +20,6 @@ public class InscricaoController {
     public Inscricao cadastrarInscricao(@RequestBody Inscricao inscricao) {
         return inscricaoService.salvarInscricao(inscricao);
     }
-}
-
-    @Autowired
-    InscricaoService inscricaoService;
 
     @GetMapping("buscar")
     public List<Inscricao> buscarInscricao() {
--- maratona/src/main/java/com/example/maratona/Controller/MaratonaController.java
+++ maratona/src/main/java/com/example/maratona/Controller/MaratonaController.java
@@ -5,26 +5,12 @@ import com.example.maratona.Entity.Maratona;
 import com.example.maratona.Service.InscricaoService;
 import com.example.maratona.Service.MaratonaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/maratona")
-import com.example.maratona.Entity.Inscricao;
-import com.example.maratona.Entity.Maratona;
-import com.example.maratona.Service.CircuitoService;
-import com.example.maratona.Service.MaratonaService;
-import com.example.maratona.Service.MaratonistaService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-@RestController
-@RequestMapping
 public class MaratonaController {
 
     @Autowired
@@ -34,11 +20,6 @@ public class MaratonaController {
     public Maratona cadastrarMaratona(@RequestBody Maratona maratona) {
         return maratonaService.salvarMaratona(maratona);
     }
-
-}
-
-    @Autowired
-    MaratonaService maratonaService;
 
     @GetMapping("buscar")
     public List<Maratona> buscarMaratona() {
